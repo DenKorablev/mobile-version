83ffa325ccbfbe5982e162012ec227d457322e6f
denis.korablev <denis.korablev@indusoft.ru>
2018-09-14 17:02:45 +0300

    проект мобильной версии

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nav-menu',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent {
+
   items =["Приложение", "ПВД", "Отгрузка", "Мнемосхемы SVG"];
 }
--- src/app/profile-menu/profile-menu.component.ts
+++ src/app/profile-menu/profile-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-profile-menu',
@@ -10,4 +10,17 @@ export class ProfileMenuComponent {
   profileItems = ["На главную страницу", "Избранное", "Перейти в полную версию", "Уведомление"];
   settingItems = ["Профиль пользователя", "Настройки пользователя", "Панель администрирования"]; 
   modeItems = ["Перейти в полноэкранный режим", "Показать информационную панель"];
+
+  sizeLightboxBlock(): any {
+    let header = document.querySelector('header').clientHeight;
+    return { "width": `${header}px` };
+  }
+
+  @HostListener('window:resize', ['$event'])
+  sizeContentBlock(): any {
+    let header = document.querySelector('header').clientHeight;
+    let height = window.innerHeight - header * 2;
+    let width = window.outerWidth - header;
+    return { "height": `${height}px`, "width": `${width}px` };
+  }
 }
